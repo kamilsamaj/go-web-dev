a43f8ff0c22aad9b1637e0b212ed9e8e58dd6f2a
kamilsamaj <kamilsamaj@gmail.com>
2021-10-02 10:02:50 +0200

    set Content-Type header text/plain

--- 09-http-server/04-custom-headers/main.go
+++ 09-http-server/04-custom-headers/main.go
@@ -9,10 +9,12 @@ type myHandler int
 
 func (h myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("X-Custom-Header", "This is a value of a custom header")
+	w.Header().Set("Content-Type", "text/plain")
 	fmt.Fprintf(w, "<h1>This is a custom HTTP response</h1>")
 }
 
 func main() {
 	var h myHandler
+	fmt.Println("Starting server on http://localhost:8080")
 	http.ListenAndServe(":8080", h)
 }
